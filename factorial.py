dde1db3923ff043c3245b16b93f3ce43f1fec61b
Ubuntu <ubuntu@ip-172-31-57-171.ec2.internal>
2021-05-21 18:16:43 +0000

    added factorial.py

---
+++ factorial.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# Created by: Layla Michel
+# Created on: May 21, 2021
+# This program asks the user to input a number and then
+# calculates and displays the factorial of that number
+
+number_as_string = input("Enter a whole number: ")
+
+
+def main():
+    # define variables
+    factorial = 1
+    counter = 0
+
+    try:
+        # convert from string to integer
+        number_as_int = int(number_as_string)
+
+        if (number_as_int < 0):
+            print("{} is not a whole number.". format(number_as_int))
+        elif (number_as_int == 0):
+            print("0! = 1")
+        else:
+            while True:
+                counter = counter + 1
+                factorial = factorial * counter
+                print("Tracking {} times through loop.". format(counter))
+
+                if (counter >= number_as_int):
+                    break
+
+            print("")
+            print("{0}! = {1}". format(number_as_int, factorial))
+
+    except ValueError:
+        print("{} is not a whole number.". format(number_as_string))
+
+
+if __name__ == "__main__":
+    main()
